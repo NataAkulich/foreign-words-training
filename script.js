2970c8dd54cc81053e0aaff66b1a510fb6147900
NataAkulich <110620687+NataAkulich@users.noreply.github.com>
2022-12-20 18:55:17 +0300

    .

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ const worlds = {
 
 const container = document.querySelector('#card-front');
 container.textContent = worlds.EnWorlds[OneWord];
-const cards = document.querySelector('.flip-card');
 
 function upendCard() {
     this.classList.toggle('flip');
 }
-cards.addEventListener('click', upendCard);
+newFlipcard.addEventListener('click', upendCard);
 
 const containerOne = document.querySelector('#card-back');
 containerOne.textContent = worlds.RusWorlds[OneWord];
@@ -25,6 +24,7 @@ containerOne.textContent = worlds.RusWorlds[OneWord];
 
 const NextCard = document.querySelector("#next");
 const BackCard = document.querySelector("#back");
+const ExampleCard = document.querySelector('#card-back span');
 
 
 
@@ -33,23 +33,19 @@ const currentWord = document.querySelector("#current-word");
 const WordsProgress = document.querySelector("#words-progress");
 const ExamProgress = document.querySelector("#exam-progress");
 const totalWord = document.querySelector("#total-word");
-
+totalWord.textContent = sizeArray;
 
 
 function getProgress(i) {
     return (100 * (i + 1)) / sizeArray;
 }
-container.textContent = worlds.EnWorlds[0];
-containerOne.textContent = worlds.RusWorlds[0];
-currentWord.textContent = worlds.examples[0];
-WordsProgress.value = getProgress(0);
-currentWord.textContent = 1;
-totalWord.textContent = sizeArray;
+
+
 newFlipcard.addEventListener('click', () => {
-    if (newFlipcard.classList.contains("active")) {
-        newFlipcard.classList.remove("active");
-    } else {
-        newFlipcard.classList.add("active");
+   if (newFlipcard.classList.contains('active')) {
+        newFlipcard.classList.remove('active');
+   } else {
+        newFlipcard.classList.add('active');
     }
 
 })
@@ -60,11 +56,11 @@ NextCard.addEventListener('click', () => {
     } else {
         OneWord++;
         currentWord.textContent = OneWord + 1;
-        WordsProgress.value = getProgress(OneWord);
+        WordsProgress.value = getWordsProgress(OneWord);
         BackCard.disabled = false;
         container.textContent = worlds.EnWorlds[OneWord];
-        BackCard.textContent = worlds.RusWorlds[OneWord];
-        currentWord.textContent = worlds.examples[OneWord];
+        containerOne.textContent = worlds.RusWorlds[OneWord];
+        ExampleCard.textContent = worlds.examples[OneWord];
         if (OneWord >= sizeArray - 1) {
             NextCard.disabled = true;
         }
@@ -80,8 +76,8 @@ BackCard.addEventListener('click', () => {
         currentWord.textContent = OneWord + 1;
         NextCard.disabled = false;
         container.textContent = worlds.EnWorlds[OneWord];
-        BackCard.textContent = worlds.RusWorlds[OneWord];
-        currentWord.textContent = worlds.examples[OneWord];
+        containerOne.textContent = worlds.RusWorlds[OneWord];
+        ExampleCard.textContent = worlds.examples[OneWord];
         if (OneWord <= 0) {
             BackCard.disabled = true;
         }
